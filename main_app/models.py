050f63da56f60aa2130a7e237a0a5aaf49d20b2a
damarisfreg <fregosodmar24@gmail.com>
2023-09-29 15:20:20 -0700

    Post Model

--- main_app/models.py
+++ main_app/models.py
@@ -8,3 +8,11 @@ class Pet(models.Model):
     breed = models.CharField(max_length=50)
     description = models.TextField(max_length=250)
     profile_picture = models.CharField(max_length=200)
+
+
+class Post(models.Model):
+    image = models.CharField(max_length=250)
+    caption = models.TextField(max_length=250)
+    likes = models.IntegerField()
+    date = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
