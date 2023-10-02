e634c8d4614a7bec791a26e4bb2c99c3334d1d1f
chrispaladin7 <chrismartey@yahoo.com>
2023-10-02 17:11:27 -0400

    implements Pets creation

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
 	path('', views.home, name='home'),
   path('pets/', views.pets_list, name='pets_list'),
-  # path('pets/create/', )
+  path('pets/create/', views.PetCreate.as_view(),name='pets_create')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,CreateView
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Pet 
 
@@ -7,8 +8,19 @@ from .models import Pet
 def home(request):
   return render(request, 'home.html')
 
+@login_required
 def pets_list(request):
   pets = Pet.objects.filter(user=request.user)
   return render (request, 'pets/pets_list.html', {
     'pets': pets
   })
+
+
+class PetCreate(LoginRequiredMixin,CreateView):
+  model = Pet
+  fields = ['name','type_of_animal','breed','description','profile_picture']
+  success_url='/pets'
+
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
