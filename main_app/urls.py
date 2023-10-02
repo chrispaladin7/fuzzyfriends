72179741f3f7e6a5bb26b9c0fcd8b7e2661efc6b
damarisfreg <fregosodmar24@gmail.com>
2023-10-02 10:37:44 -0700

    list of pets showing

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 	
 urlpatterns = [
 	path('', views.home, name='home'),
-    
+  path('pets/', views.pets_list, name='pets_list'),
+  # path('pets/create/', )
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Pet 
 
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
+
+def pets_list(request):
+  pets = Pet.objects.filter(user=request.user)
+  return render (request, 'pets/pets_list.html', {
+    'pets': pets
+  })
