5be5f754d6b5d73348193034894fd7f6c5dc49cc
chrispaladin7 <chrismartey@yahoo.com>
2023-10-04 17:00:57 -0400

    fixes comments on post

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,15 +1,17 @@
 from django import forms
-from .models import Post, Pet
+from .models import Post, Pet, Comment
 
 class PostForPetForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['pet', 'caption']
-        # widgets = {
-        #     'image': forms.File
-        # }
 
     def __init__(self, *args, **kwargs):
         request = kwargs.pop('request', None)
         super().__init__(*args, **kwargs)
         self.fields['pet'].queryset = Pet.objects.filter(user=request.user)
+
+class CommentsForPost(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['comment']
--- main_app/urls.py
+++ main_app/urls.py
@@ -1,20 +1,20 @@
 from django.urls import path
 from . import views
-
+	
 urlpatterns = [
-    path('', views.home, name='home'),
-    # pets
-    path('pets/', views.pets_list, name='pets_list'),
-    path('pets/create/', views.PetCreate.as_view(), name='pets_create'),
-    path('pets/<int:pk>/update/', views.PetUpdate.as_view(), name='pets_update'),
-    path('pets/<int:pk>/delete/', views.PetDelete.as_view(), name='pets_delete'),
-    # post
-    path('post/create/', views.PostCreate.as_view(), name='post_create'),
-    path('posts/<int:pk>/update/', views.PostUpdate.as_view(), name='posts_update'),
-    path('posts/<int:pk>/delete/', views.PostDelete.as_view(), name='posts_delete'),
-    path('posts/', views.posts_list, name='posts_list'),
-    # Comments
-    path('comments/<int:post_id>/create/', views.add_comment, name='add_comment'),
-    # signup
-    path('accounts/signup/', views.signup, name='signup'),
+	path('', views.home, name='home'),
+  #pets
+  path('pets/', views.pets_list, name='pets_list'),
+  path('pets/create/', views.PetCreate.as_view(),name='pets_create'),
+  path('pets/<int:pk>/update/', views.PetUpdate.as_view(), name='pets_update'),
+  path('pets/<int:pk>/delete/', views.PetDelete.as_view(), name='pets_delete'),
+  #post
+  path('post/create/', views.PostCreate.as_view(), name='post_create'),
+  path('posts/<int:pk>/update/', views.PostUpdate.as_view(), name='posts_update'),
+  path('posts/<int:pk>/delete/', views.PostDelete.as_view(), name='posts_delete'),
+  path('posts/', views.posts_list, name='posts_list'),
+  # Comments
+  path('comments/<int:post_id>/create', views.add_comment, name='add_comment'),
+  #signup
+  path('accounts/signup/', views.signup, name='signup'),
 ]
