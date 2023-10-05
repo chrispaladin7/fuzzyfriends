bad769b99bc9a14d1b6de21cb282fc0340e7d4b2
Artin-Simonian <artinsimonian@yahoo.com>
2023-10-05 16:12:05 -0700

    Finalizing app

--- main_app/views.py
+++ main_app/views.py
@@ -164,32 +164,6 @@ def signup(request):
     context = {'form': form, 'error_message': error_message}
     return render(request, 'registration/signup.html', context)
 
-# def post_like(request,pk):
-#     post = get_object_or_404(Post, id=request.POST.get('blogpost_id'))
-#     if post.likes.filter(id=request.user.id).exists():
-#         post.likes.remove(request.user)
-#     else:
-#         post.likes.add(request.user)
-
-#     # return render(request, 'home')
-
-#     return HttpResponseRedirect(reverse('home', args=[str(pk)]))
-
-# class BlogPostDetailView(DetailView):
-#     model = Post
-#     # template_name = MainApp/BlogPost_detail.html
-#     # context_object_name = 'object'
-
-#     def get_context_data(self, **kwargs):
-#         data = super().get_context_data(**kwargs)
-
-#         likes_connected = get_object_or_404(Post, id=self.kwargs['pk'])
-#         liked = False
-#         if likes_connected.likes.filter(id=self.request.user.id).exists():
-#             liked = True
-#         data['number_of_likes'] = likes_connected.number_of_likes()
-#         data['post_is_liked'] = liked
-#         return data
 
 @login_required
 def posts_like(request, post_id):
